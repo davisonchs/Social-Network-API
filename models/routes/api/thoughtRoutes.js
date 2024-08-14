a6fc462845ea54b11e56810c8a6aa8c4e227b0c3
Davison Hernandez <davisonchs@live.com>
2024-08-14 01:35:39 -0500

    added routes

---
+++ models/routes/api/thoughtRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { getThoughts, getThoughtById, createThought, updateThought, deleteThought } = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+module.exports = router;
---
+++ models/routes/api/userRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
